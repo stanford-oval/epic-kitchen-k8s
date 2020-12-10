f9517c7336ecb433fa2212ddc9365ccc4564580b
Jackie Yang <jackie@jackieyang.me>
2020-12-09 20:31:08 -0800

    fix bash scripts

--- download-dataset.sh
+++ download-dataset.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/zsh
 set -ex
 
-if [! -d "/data/epic-kitchens-100-annotations" ]
+if [ ! -d "/data/epic-kitchens-100-annotations" ]
 then
 	git clone https://github.com/epic-kitchens/epic-kitchens-100-annotations.git
 fi
 
-if [! -d "/data/epic-kitchen-all" ]
+if [ ! -d "/data/epic-kitchen-all" ]
 then
 	pushd /data/epic-kitchen-all
 	aws s3 cp --recursive s3://geniehai/jackiey/dataset/ ./
--- download-pretrained.sh
+++ download-pretrained.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 set -ex
 
-if [! -d "./epic-kitchen-lstm/pretrained" ]
+if [ ! -d "./epic-kitchen-lstm/pretrained" ]
 then
 	pushd epic-kitchen-lstm
 	aws s3 cp --recursive s3://geniehai/jackiey/pretrained/ ./pretrained/
